439db34377fd37e7425a557925e1a7a8cf2a4c2a
OnlyOne <dzbik.konrad@gmail.com>
2018-03-08 21:49:05 +0100

    dodanie dwóch parametrów, umożliwiających definicję connectionString oraz dodanie paramatru odpowiedzialnego za schemat bazy danych; //TODO: przykladowe wykresy

--- WebApplication1/Controllers/ChartController.cs
+++ WebApplication1/Controllers/ChartController.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using WebApplication1.Models;
 
@@ -14,28 +12,31 @@ namespace WebApplication1.Controllers
         public DataTable results = new DataTable();
         public DataTable resultsTrend = new DataTable();
         public DataTable resultsHistoria = new DataTable();
+        private static string _schemaName; 
 
+        //public string connString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=charts.drawing;Integrated Security=True";
+        public string connString = null;//"Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=charts.drawing;Integrated Security=True";
 
-        public string connString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=charts.drawing;Integrated Security=True";
 
         //zapytania pomocnicze
         public string queryIPT = "select distinct IPT from GodzinyKontowaneP_Podsumowanie where xIPT = 1 order by IPT";
-        public string queryPeriod = "select distinct Period_Year Parametr,left(Period_Year, 4)+'-'+right(Period_Year,2) Etykieta from GodzinyKontowaneP_Podsumowanie where Period_Year is not null order by Etykieta desc";
+        public string queryPeriod = "select distinct Period_Year Parametr,left(Period_Year, 4)+'-'+right(Period_Year,2) Etykieta from " + _schemaName + "GodzinyKontowaneP_Podsumowanie where Period_Year is not null order by Etykieta desc";
 
         //gotowe zapytanie pod jeden wykres a dokladnie TREND
         public string queryTrend = "select * ,[SBRK] + [SLHIPA] as PośrednioProdukcyjne,[SLHDPA] as BezpośrednioProdukcyjne,[SOT] as Nadgodziny,[SLT] as Nieobecności,[SBRK] + [SLHIPA] + [SLHDPA] as Wszystkie,([SBRK] + [SLHIPA])/([SBRK] + [SLHIPA] + [SLHDPA]) DOIi " +
-                                    "from GodzinyKontowaneP_Podsumowanie," +
+                                    "from "+ _schemaName+"GodzinyKontowaneP_Podsumowanie," +
                                     "(select distinct Period_Year Parametr,left(Period_Year, 4)+'-'+right(Period_Year,2) Etykieta from GodzinyKontowaneP_Podsumowanie where Period_Year is not null ) as periodsProcedure " +
                                     "where xIPT = 1 and x = 2 and Period_Year = periodsProcedure.Parametr;";
 
         //zapytanie gotowe dla HISTORIA
         public string queryHistoria = "select *, [SBRK] + [SLHIPA] as PośrednioProdukcyjne, [SLHDPA] as BezpośrednioProdukcyjne, [SOT] as Nadgodziny, [SLT] as Nieobecności, [SBRK] + [SLHIPA] + [SLHDPA] as Wszystkie, ([SBRK] + [SLHIPA])/([SBRK] + [SLHIPA] + [SLHDPA]) DOIi " +
-                                      "from GodzinyKontowaneP_Podsumowanie," +
+                                      "from " + _schemaName + "GodzinyKontowaneP_Podsumowanie," +
                                       "(select distinct Period_Year Parametr,left(Period_Year, 4)+'-'+right(Period_Year,2) Etykieta from GodzinyKontowaneP_Podsumowanie where Period_Year is not null ) as periodsProcedure " +
                                       "where Period_Year = periodsProcedure.Parametr and xIPT =1 order by C";
 
         public ActionResult Index()
         {
+            GetConnectionStringAndSchemaIfExist();
             TradingModel objProductModel = new TradingModel();
             objProductModel.ProductData = new Product();
             objProductModel.ProductData = GetChartData();
@@ -53,6 +54,7 @@ namespace WebApplication1.Controllers
 
         public ActionResult Chart1()
         {
+            GetConnectionStringAndSchemaIfExist();
             TradingModel objProductModel = new TradingModel();
             objProductModel.ProductData = new Product();
             objProductModel.ProductData = GetChartData();
@@ -69,6 +71,7 @@ namespace WebApplication1.Controllers
         }
         public ActionResult Chart2()
         {
+            GetConnectionStringAndSchemaIfExist();
             TradingModel objProductModel = new TradingModel();
             objProductModel.ProductData = new Product();
             objProductModel.ProductData = GetChartData();
@@ -115,6 +118,13 @@ namespace WebApplication1.Controllers
             using (SqlCommand command = new SqlCommand(queryTrend, conn))
             using (SqlDataAdapter dataAdapter = new SqlDataAdapter(command))
                 dataAdapter.Fill(resultsHistoria);
+        }
+
+        public void GetConnectionStringAndSchemaIfExist()
+        {
+            connString = ConfigurationManager.AppSettings["connectionString"];
+            _schemaName = ConfigurationManager.AppSettings["schemaName"];
+
         }
     }
 }
