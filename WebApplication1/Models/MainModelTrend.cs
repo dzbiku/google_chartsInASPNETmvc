8d1eb49c2107acf6d8958d71dacfa0bdcaee659d
OnlyOne <dzbik.konrad@gmail.com>
2018-03-08 22:47:10 +0100

    start with models

--- WebApplication1/Models/MainModelTrend.cs
+++ WebApplication1/Models/MainModelTrend.cs
@@ -5,8 +5,58 @@ using System.Web;
 
 namespace WebApplication1.Models
 {
+    //GodzinyKontowaneP
     public class MainModelTrend
     {
-        public int id { get; set; }
+        public DateTime? Od { get; set; }
+        public DateTime? Do { get; set; }
+        public int? C { get; set; }
+        public int? O { get; set; }
+        public DateTime? Data { get; set; }
+        public int? Period { get; set; }
+        public int? Rok { get; set; }
+        public string Ipt { get; set; }
+        public double? Brk { get; set; }
+        public double? Lhipa { get; set; }
+        public double? Lhdpa { get; set; }
+        public double? Ot { get; set; }
+        public double? Lt { get; set; }
+        public double? Sbrk { get; set; }
+        public double? Slhipa { get; set; }
+        public double? Slhdpa { get; set; }
+        public double? Sot { get; set; }
+        public double? Slt { get; set; }
+        public double? PlanWyk { get; set; }
+        public double? PlanWykD { get; set; }
+        public double? ProgWyk { get; set; }
+        public double? Trend { get; set; }
+        public double? MinCzas { get; set; }
+        public double? Roznica { get; set; }
+        public int? X { get; set; }
+        public double? PlanDOI { get; set; }
+        public double? PlanOT { get; set; }
+        public double? PlanLT { get; set; }
+        public double? pDOI { get; set; }
+        public double? pOT { get; set; }
+        public double? pLT { get; set; }
+        public double? yPlanDOI { get; set; }
+        public double? yPlanOT { get; set; }
+        public double? yPlanLT { get; set; }
+        public double? yDOI { get; set; }
+        public double? yOT { get; set; }
+        public double? yLT { get; set; }
+        public int? xIPT { get; set; }
+        public int? Period_Year { get; set; }
+        public double? DOI { get; set; }
+    }
+
+    public class TrendModel
+    {
+
+    }
+
+    public class HistoryModel
+    {
+
     }
 }
