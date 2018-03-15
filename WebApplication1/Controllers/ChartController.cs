a962664d6587f5a729957bbcdc9b50ab269130b3
OnlyOne <dzbik.konrad@gmail.com>
2018-03-15 23:07:11 +0100

    some change, don't remember why I do it, but nice to have :)

--- WebApplication1/Controllers/ChartController.cs
+++ WebApplication1/Controllers/ChartController.cs
@@ -5,6 +5,9 @@ using System.Data.SqlClient;
 using System.Web.Mvc;
 using WebApplication1.Models;
 using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 namespace WebApplication1.Controllers
 {
@@ -59,18 +62,13 @@ namespace WebApplication1.Controllers
         public ActionResult Chart1()
         {
             GetConnectionStringAndSchemaIfExist();
-            TradingModel objProductModel = new TradingModel();
-            objProductModel.ProductData = new Product();
-            objProductModel.ProductData = GetChartData();
+            HistoryModel objProductModel = new HistoryModel();
+            objProductModel.HistoryDatas = new History();
+            // objProductModel.HistoryDatas = resultsHistoria;
             objProductModel.YearTitle = "Year";
             objProductModel.ImportsTitle = "Imports";
             objProductModel.ExportsTitle = "Exports";
-            //getIPT();
-            //getTrend();
-            //gethistory();
-            //var tmp = results;
-            //var tmptrend = resultsTrend;
-            //var tmphistory = resultsHistoria;
+
             return View(objProductModel);
         }
 
@@ -146,11 +144,42 @@ namespace WebApplication1.Controllers
         public JsonResult GetChartHistory()
         {
             string JSONString = string.Empty;
+            //List<History> JSONString = new List<History>();
             GetConnectionStringAndSchemaIfExist();
             gethistory();
             var tmphistory = resultsHistoria;
+            //JSONString = ConvertDataTable<History>(resultsHistoria);
+            
             JSONString = JsonConvert.SerializeObject(resultsHistoria);
             return Json(new { JSONList = JSONString }, JsonRequestBehavior.AllowGet);
+        }
+
+        private static List<T> ConvertDataTable<T>(DataTable dt)
+        {
+            List<T> data = new List<T>();
+            foreach (DataRow row in dt.Rows)
+            {
+                T item = GetItem<T>(row);
+                data.Add(item);
+            }
+            return data;
+        }
+        private static T GetItem<T>(DataRow dr)
+        {
+            Type temp = typeof(T);
+            T obj = Activator.CreateInstance<T>();
+
+            foreach (DataColumn column in dr.Table.Columns)
+            {
+                foreach (PropertyInfo pro in temp.GetProperties())
+                {
+                    if (pro.Name == column.ColumnName)
+                        pro.SetValue(obj, dr[column.ColumnName], null);
+                    else
+                        continue;
+                }
+            }
+            return obj;
         }
     }
 }
